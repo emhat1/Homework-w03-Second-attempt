b92b1d6cb4f0d9e75f97640086773fd2a9ce5ac9
Erin H <e.hatherell@gmail.com>
2021-12-09 22:09:13 +1100

    Working on getting password to appear onscreen

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-//Arrays for all possible characters to be included in password
-var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var number = ['0','1','2','3','4','5','6','7','8','9'];
-var symbol = ['~','!','@','#','$','%','^','&','*','(',')','_','+','`','-','=','/','.',','];
-
-//Prompt for character types
-var includeUpperCase = confirm("Do you want to include upper case letters?");
-var includeLowerCase = confirm("Do you want to include lower case letters?");
-var includeNumber = confirm("Do you want to include numbers?");
-var includeSymbol = confirm("Do you want to include special characters?");
-
-var passLength = window.prompt("Enter password length (8-128 characters)");
-
-
 
 function generateOptions() {
-    // If unacceptable response given, create an alert
+  //Arrays for all possible characters to be included in password
+  var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var number = ['0','1','2','3','4','5','6','7','8','9'];
+  var symbol = ['~','!','@','#','$','%','^','&','*','(',')','_','+','`','-','=','/','.',','];
+
+  //Prompt for character types
+  var includeUpperCase = confirm("Do you want to include upper case letters?");
+  var includeLowerCase = confirm("Do you want to include lower case letters?");
+  var includeNumber = confirm("Do you want to include numbers?");
+  var includeSymbol = confirm("Do you want to include special characters?");
+
+  var passLength = window.prompt("Enter password length (8-128 characters)");
+
+ 
+  // If unacceptable response given, create an alert
   if (!(passLength >=8 && passLength <=128)) {
     alert("That password length is not an answer I can use");
     return;
@@ -51,19 +51,19 @@ function generateOptions() {
    var charSelect = Math.floor(Math.random() * characterPool.length);
     finalPass.push(characterPool[charSelect])
   }
+  // Write password to the #password input
+  function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.textContent = password;
+  }
 }
-
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.textContent = password;
 
-}
+
 
 // Add event listener to button
 generateBtn.addEventListener("click", generateOptions);

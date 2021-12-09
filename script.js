60e873a5cb0e48099db0ae119f1b69d21376729e
Erin H <e.hatherell@gmail.com>
2021-12-09 19:23:41 +1100

    Tinker tinker tinker

--- script.js
+++ script.js
@@ -71,8 +71,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  passwordText.textContent = password;
 
 }
 
